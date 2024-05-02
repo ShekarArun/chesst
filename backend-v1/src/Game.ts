59759ccb45528fc275e07919dee6b68cd522a73a
Arun Shekar <shekar.arun@gmail.com>
2024-05-02 23:22:17 +0530

    feat: Add logic to make and validate moves

--- backend-v1/src/Game.ts
+++ backend-v1/src/Game.ts
@@ -1,30 +1,52 @@
+import { Chess } from "chess.js";
 import { WebSocket } from "ws";
+import { GAME_OVER } from "./messages";
 
 export class Game {
     public player1: WebSocket;
     public player2: WebSocket;
-    private board: String;
-    private moves: String[];
+    private board: Chess;
     private startTime: Date;
 
     constructor(player1: WebSocket, player2: WebSocket) {
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.moves = [];
+        this.board = new Chess();
         this.startTime = new Date();
     }
 
-    makeMove(socket: WebSocket, move: String) {
-        // Validate:
-        // 1. Is it this user's turn?
-        // 2. Is the move valid?
+    makeMove(socket: WebSocket, move: {
+        from: string,
+        to: string
+    }) {
+        // TODO: Validate type of 'move' using zod
 
+        // Is it this user's turn?
+        if (this.board.moves.length % 2 === 0 && socket !== this.player1) {
+            return;
+            // TODO: Raise 'not your turn' error
+        }
+        if (this.board.moves.length % 2 === 1 && socket !== this.player2) {
+            return;
+            // TODO: Raise 'not your turn' error
+        }
 
-        // Update board
-        // Push move
+        try {
+            this.board.move(move) // Handles move validation and updates the board if valid
+        } catch (e) {
+            console.error('Error making this move!', e)
+            throw e
+        }
 
         // Check if game over
+        if (this.board.isGameOver()) {
+            this.player1.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === 'w' ? 'black' : 'white'
+                }
+            }))
+        }
         // Send updated board to both players
     }
 }
